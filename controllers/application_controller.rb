730fd71fb0379cb14c0c15ca2baa1fdab35542dc
gizele22 <www.gizele.com@gmail.com>
2019-08-20 17:51:06 +0800

    webgame update

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -18,8 +18,46 @@ class ApplicationController < Sinatra::Base
   get '/level' do
     @game_level = params['game_level']
 
-    puts @game_level
+    session['guessed_number'] = nil
+    session['secret_number'] = nil
 
     erb :level
+  end
+
+  post '/level' do
+    session['level'] = params['game_level']
+    redirect to('/game')
+  end
+
+  get '/game' do
+    session['secret_number'] = secret_number(session['level'])
+    erb :game
+  end
+
+  post '/game' do
+    session['guessed_number'] = params['guessed_number']
+    if session['guessed_number'].to_i == session['secret_number']
+      erb :result
+    else
+      erb :game
+    end
+  end
+
+  get '/result' do
+    erb :result
+  end
+
+  post '/result' do
+    session['game'] = params['guessed_number']
+  end
+
+  def secret_number(level)
+    if level == 'easy'
+      rand(1..20)
+    elsif level == 'medium'
+      rand(21..40)
+    else level == 'hard'
+      rand(41..77)
+    end
   end
 end

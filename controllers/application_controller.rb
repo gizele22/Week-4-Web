6cbcde35c844d6967451387235eb01c4987af2be
gizele22 <www.gizele.com@gmail.com>
2019-08-21 17:16:44 +0800

    updated web game

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -26,6 +26,10 @@ class ApplicationController < Sinatra::Base
 
   post '/level' do
     session['level'] = params['game_level']
+
+    session['total_chances'] = 6
+    session['counter'] = 0
+
     redirect to('/game')
   end
 
@@ -36,10 +40,16 @@ class ApplicationController < Sinatra::Base
 
   post '/game' do
     session['guessed_number'] = params['guessed_number']
-    if session['guessed_number'].to_i == session['secret_number']
+    if number_matched_win
       erb :result
     else
-      erb :game
+      session['counter'] += 1
+
+      if session['counter'] == session['total_chances']
+        erb :over
+      else
+        erb :game
+      end
     end
   end
 
@@ -51,6 +61,7 @@ class ApplicationController < Sinatra::Base
     session['game'] = params['guessed_number']
   end
 
+private
   def secret_number(level)
     if level == 'easy'
       rand(1..20)
@@ -59,5 +70,34 @@ class ApplicationController < Sinatra::Base
     else level == 'hard'
       rand(41..77)
     end
+  end
+
+  def run_game
+    3
+  end
+
+  def count
+
+    @count = 0
+    session['level'].to_i > session['guessed_number'] = @count
+    if session[guessed_number] = session['secret_number']
+    erb :result
+  else
+    erb :game
+  end
+end
+
+  def number_matched_win
+    session['guessed_number'].to_i == session['secret_number']
+  end
+
+  post '/index' do
+    session['name'] = params['age']
+    if game_end
+      erb :result
+    end
+
+  def game_end
+    session[@player_name].to_i == record['age']
   end
 end
